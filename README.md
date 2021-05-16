4fd7f9d4fe8dc41d7b2010196b6486a8248320de
Grzegorz <g.matkowski@pokato.pl>
2021-05-16 09:55:32 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# SeniorCare
+
+###Requirements:
+- Docker or Docker Desktop
+
+###Installation guide:
+- Oh fuck this ... just run ./start.sh
+
+Then simply visit https://seniorcare.test
+
+If SSL certificats not working simple add them to you certificats DB (./docker/nginx/ssl/seniorcare.test.crt)
---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+docker-compose build && docker-compose up -d
+
+if [ ! -f ./.env ] && [ -f ./.env.dev ];
+then
+    cp ./.env.dev ./.env
+fi
+
+if [ -f ./composer.json ];
+then
+    docker exec -i seniorcare_php bash -c "composer install"
+fi
+
+if [ $? -ne 0 ]
+then
+    printf "${RED}Dockerized ! Something wrong happen, check output! :-("
+else
+    printf "${GREEN}Dockerized ! You can now start hacking! :-)"
+fi
