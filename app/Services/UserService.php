0feaeaf0199cdacb0bfea382d3832d877ef0d309
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2021-06-09 19:11:22 +0200

    test

---
+++ app/Services/UserService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserService
+{
+    //
+}
