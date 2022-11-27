a5f0ae24513d62d9b33aab450ee52473b3f86507
Grzegorz <g.matkowski@pokato.pl>
2022-11-27 15:43:18 +0100

    Fix docker-compose db

--- docker/postgres/initdb.sh
+++ docker/postgres/initdb.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
+
 set -e
 set -u
 
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER <<-EOSQL
-	    CREATE USER $database WITH PASSWORD '${database//\"}';
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database;
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
-
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
